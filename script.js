4afec5e7d9a2f00e02374b10ccbe7e5c4a96384b
Hatice06 <haticeaydogan00@gmail.com>
2022-02-21 19:40:24 +0000

    Initial commits

--- script.js
+++ script.js
@@ -2,18 +2,17 @@
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
-  let oneShow = getOneShow();
-  let divEl = document.createElement("div");
-  let pEl = document.createElement("p");
-  pEl.innerText = oneShow.name;
-  divEl.appendChild(pEl);
-  const rootElem = document.getElementById("root");
-  rootElem.appendChild(divEl);
 }
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  let oneShow = getOneShow();
+  // let divEl = document.createElement("div");
+  // let pEl = document.createElement("p");
+  // pEl.innerText = oneShow.name;
+  // divEl.appendChild(pEl);
+  // rootElem.appendChild(divEl);
+  //rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  rootElem.textContent = oneShow.name;
 }
-
 
 window.onload = setup;
