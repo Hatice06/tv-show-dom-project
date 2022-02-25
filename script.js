49472fd8feede5a515ab9d67ebed98017c206180
Hatice06 <haticeaydogan00@gmail.com>
2022-02-25 23:11:39 +0000

    removing unnecessary codes

--- script.js
+++ script.js
@@ -28,16 +28,18 @@ homeButton.innerText = "HOME";
 searchDiv.append(homeButton ,episodeSelector, inputEl, searchCounter);
 rootEl.append(searchDiv, episodeContainer);
 
+const allEpisodes = getAllEpisodes();
 
 //render page on the window load;
 function setup() {
-  const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
   searchCounter.innerText = `Displaying ${allEpisodes.length} / ${allEpisodes.length} episodes`
 }
 
 //build page for all episodes;
 function makePageForEpisodes(episodeList) {
+  episodes.innerHTML = "";
+
   episodeList.map((episode) => {
     let episodeCard = document.createElement("div");
     let episodeTitle = document.createElement("h3");
@@ -66,61 +68,38 @@ function makePageForEpisodes(episodeList) {
     }`;
 
     episodeSelector.appendChild(optionEl);
-  });
-
-  //search episodes event listener
-  inputEl.addEventListener("keyup", () => {
-    episodes.innerHTML = "";
-
-    let filteredEpisodes = episodeList.filter(
-      (episode) =>
-        episode.summary.toLowerCase().includes(inputEl.value.toLowerCase()) ||
-        episode.name.toLowerCase().includes(inputEl.value.toLowerCase())
-    );
-
-    searchCounter.innerText = `Displaying ${filteredEpisodes.length} / ${episodeList.length} episodes`;
-    showFilteredEpisodes(filteredEpisodes);
-  });
-
-  let episodeDropDown = document.getElementById("episode-select")
-  episodeDropDown.addEventListener("change", (event) => {
-    const episodeId = event.target.value;
-    
-    const episodeFindById = episodeList.filter(
-      (episode) => episode.id == episodeId
-    );
-    showFilteredEpisodes(episodeFindById);
-  });
+  }); 
 }
 
+//search episodes event listener
+inputEl.addEventListener("keyup", () => {
+  let filteredEpisodes = allEpisodes.filter(
+    (episode) =>
+    episode.summary.toLowerCase().includes(inputEl.value.toLowerCase()) ||
+    episode.name.toLowerCase().includes(inputEl.value.toLowerCase())
+    );
+    
+    searchCounter.innerText = `Displaying ${filteredEpisodes.length} / ${allEpisodes.length} episodes`;
+    makePageForEpisodes(filteredEpisodes);
+  });
+
+//episode selector
+let episodeDropDown = document.getElementById("episode-select");
+episodeDropDown.addEventListener("change", (event) => {
+  const episodeId = event.target.value;
+
+  const episodeFindById = allEpisodes.filter(
+    (episode) => episode.id == episodeId
+  );
+  makePageForEpisodes(episodeFindById);
+  searchCounter.innerText = `Displaying ${1} / ${allEpisodes.length} episodes`;
+});
 
 //episode title format
 function formatSeriesAndEpisode(episodeTitle, episode) {
   episodeTitle.innerText = `${episode.name} - S${episode.season
     .toString()
     .padStart(2, "0")}E${episode.number.toString().padStart(2, "0")}`;
-}
-
-//render page after filtered
-function showFilteredEpisodes(filteredEpisodes) {
-  episodes.innerHTML = "";
-
-  filteredEpisodes.map((episode) => {
-    let episodeCard = document.createElement("div");
-    let episodeTitle = document.createElement("h3");
-    let episodeSummary = document.createElement("p");
-    let episodeImg = document.createElement("img");
-
-    episodeCard.className = "episode-card";
-    episodeTitle.className = "episode-title";
-    episodeImg.src = episode.image.medium;
-
-    formatSeriesAndEpisode(episodeTitle, episode);
-
-    episodeSummary.innerHTML = episode.summary;
-    episodeCard.append(episodeTitle, episodeImg, episodeSummary);
-    episodes.append(episodeCard);
-  });
 }
 
 homeButton.addEventListener("click", () => {
