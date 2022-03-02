41922eb5dd26c0246a76c849206bcb51d6df4316
Hatice06 <haticeaydogan00@gmail.com>
2022-03-02 21:16:00 +0000

    Code update

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const episodeContainer = document.querySelector(".episode-container");
 const searchDiv = document.querySelector(".search-div");
 const episodes = document.querySelector(".episode-container");
 
-//create episode selector 
+//create episode selector
 const episodeSelector = document.createElement("select");
 episodeSelector.id = "episode-select";
 
@@ -24,8 +24,8 @@ searchCounter.className = "search-counter";
 let homeButton = document.createElement("button");
 homeButton.className = "home-btn";
 homeButton.innerText = "HOME";
- 
-searchDiv.append(homeButton ,episodeSelector, inputEl, searchCounter);
+
+searchDiv.append(homeButton, episodeSelector, inputEl, searchCounter);
 rootEl.append(searchDiv, episodeContainer);
 
 const allEpisodes = getAllEpisodes();
@@ -33,14 +33,14 @@ const allEpisodes = getAllEpisodes();
 //render page on the window load;
 function setup() {
   makePageForEpisodes(allEpisodes);
-  searchCounter.innerText = `Displaying ${allEpisodes.length} / ${allEpisodes.length} episodes`
+  searchCounter.innerText = `Displaying ${allEpisodes.length} / ${allEpisodes.length} episodes`;
 }
 
 //build page for all episodes;
 function makePageForEpisodes(episodeList) {
   episodes.innerHTML = "";
 
-  episodeList.map((episode) => {
+  episodeList.forEach((episode) => {
     let episodeCard = document.createElement("div");
     let episodeTitle = document.createElement("h3");
     let episodeSummary = document.createElement("p");
@@ -50,42 +50,37 @@ function makePageForEpisodes(episodeList) {
     episodeTitle.className = "episode-title";
     episodeImg.src = episode.image.medium;
 
-    formatSeriesAndEpisode(episodeTitle, episode);
+    episodeTitle.innerText = formatSeriesAndEpisode("card", episode);
 
     episodeSummary.innerHTML = episode.summary;
     episodeCard.append(episodeTitle, episodeImg, episodeSummary);
     episodes.append(episodeCard);
   });
 
-  //episode select dropdown menu
-  episodeList.map((episode) => {
+  //episode option select dropdown menu
+  episodeList.forEach((episode) => {
     let optionEl = document.createElement("option");
     optionEl.value = episode.id;
-    optionEl.innerText = `S${episode.season
-      .toString()
-      .padStart(2, "0")}E${episode.number.toString().padStart(2, "0")} -${
-      episode.name
-    }`;
+    optionEl.innerText = formatSeriesAndEpisode("option", episode);
 
     episodeSelector.appendChild(optionEl);
-  }); 
+  });
 }
 
 //search episodes event listener
 inputEl.addEventListener("keyup", () => {
   let filteredEpisodes = allEpisodes.filter(
     (episode) =>
-    episode.summary.toLowerCase().includes(inputEl.value.toLowerCase()) ||
-    episode.name.toLowerCase().includes(inputEl.value.toLowerCase())
-    );
-    
-    searchCounter.innerText = `Displaying ${filteredEpisodes.length} / ${allEpisodes.length} episodes`;
-    makePageForEpisodes(filteredEpisodes);
-  });
+      episode.summary.toLowerCase().includes(inputEl.value.toLowerCase()) ||
+      episode.name.toLowerCase().includes(inputEl.value.toLowerCase())
+  );
+
+  searchCounter.innerText = `Displaying ${filteredEpisodes.length} / ${allEpisodes.length} episodes`;
+  makePageForEpisodes(filteredEpisodes);
+});
 
 //episode selector
-let episodeDropDown = document.getElementById("episode-select");
-episodeDropDown.addEventListener("change", (event) => {
+episodeSelector.addEventListener("change", (event) => {
   const episodeId = event.target.value;
 
   const episodeFindById = allEpisodes.filter(
@@ -95,15 +90,24 @@ episodeDropDown.addEventListener("change", (event) => {
   searchCounter.innerText = `Displaying ${1} / ${allEpisodes.length} episodes`;
 });
 
-//episode title format
-function formatSeriesAndEpisode(episodeTitle, episode) {
-  episodeTitle.innerText = `${episode.name} - S${episode.season
+//episode title format(card or option)
+function formatSeriesAndEpisode(type, episode) {
+  let titleFormatCard = `${episode.name} - S${episode.season
     .toString()
     .padStart(2, "0")}E${episode.number.toString().padStart(2, "0")}`;
+
+  let titleFormatOption = `S${episode.season
+    .toString()
+    .padStart(2, "0")}E${episode.number.toString().padStart(2, "0")} - ${
+    episode.name
+  }`;
+
+  return type === "card" ? titleFormatCard : titleFormatOption;
 }
 
 homeButton.addEventListener("click", () => {
-  location.reload(true);
-})
+  setup();
+  inputEl.value = "";
+});
 
 window.onload = setup;
